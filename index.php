92188a107df9be2ff005fe2f1b41263453a5c12e
moni <almasud@w3engineers.com>
2016-04-26 12:48:55 +0600

    testing codeship integration for test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("libs/simplepie.php");
 require_once("classes/importer.php");
 
 $xmlData = file_get_contents("tests/fixtures/rss.xml");
-
+echo "testing result";
 $objImporter = new Importer();
 $arrArticles = $objImporter->importArticles($xmlData);
 var_dump($arrArticles);
