dc1bf94944922c7c88f7352bc67dada8a07ae617
moni <almasud@w3engineers.com>
2016-04-26 13:20:03 +0600

    integration on multiple branch testing

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("classes/importer.php");
 
 $xmlData = file_get_contents("tests/fixtures/rss.xml");
 echo "testing result";
+echo "testing the automated integration on Staging and Development branches";
 $objImporter = new Importer();
 $arrArticles = $objImporter->importArticles($xmlData);
 var_dump($arrArticles);
